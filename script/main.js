ce85948420b2ee795ae6d53095e35e810b348f2f
Dorian Badel <DorianBadel@protonmail.com>
2022-09-25 22:05:45 +0200

    Added the option to go in the other direction

--- script/main.js
+++ script/main.js
@@ -3,42 +3,46 @@ const sliderGalleryBottom = document.querySelector('.slider__gallery--bottom');
 const imagesTop = document.querySelectorAll('.slider__gallery--top img');
 const imagesBottom = document.querySelectorAll('.slider__gallery--bottom img');
 const btnNext = document.querySelector('.button-next');
+const btnPrev = document.querySelector('.button-previous');
 
 let focusIdTop = 1;
 let focusIdBottom = 1;
 let offsetTop = 0;
 let offsetBottom = 0;
+let isDisabledBack = false;
+let isDisabledForward = false;
 const arrayWidthTop = calculateWidthTop();
 const arrayWidthBottom = calculateWidthBottom();
 
 setInitialGalleryState();
 
-btnNext.addEventListener('click', () => {
+btnPrev.addEventListener('click', () => {
   sliderGalleryTop.style.transition = '400ms ease-in-out transform';
   sliderGalleryBottom.style.transition = '600ms ease-in-out transform';
 
-  offsetTop += imagesTop[Math.abs(focusIdTop)-1].width/2 +10;
-  offsetBottom += imagesBottom[Math.abs(focusIdBottom)-1].width/2 +10;
-
-  sliderGalleryTop.style.transform = `translateX(-${arrayWidthTop*2-offsetTop}px)`;
-  sliderGalleryBottom.style.transform = `translateX(-${arrayWidthBottom*2-offsetBottom}px)`;
-
-
-  if(focusIdTop  == 1){
-    focusIdTop = -2;
-  }else{
-    focusIdTop--;
-  }
-
-  if(focusIdBottom  == 1){
-    focusIdBottom = -2;
-  }else{
-    focusIdBottom--;
+  if(!isDisabledBack){
+    btnPrev.classList.add('disabled');
+    btnPrev.src = "./assets/images/arrow-gray-left.png";
+    isDisabledBack = true;
+    moveRowBack();
   }
 })
 
+btnNext.addEventListener('click', () => {
+  sliderGalleryTop.style.transition = '600ms ease-in-out transform';
+  sliderGalleryBottom.style.transition = '400ms ease-in-out transform';
+
+  if(!isDisabledForward){
+    btnNext.classList.add('disabled');
+    btnNext.src = "./assets/images/arrow-gray-Right.png";
+    isDisabledForward = true;
+    moveRowForward();
+  }
+})
+
+
 sliderGalleryTop.addEventListener('transitionend', () => {
-  if(Math.abs(focusIdTop)>imagesTop.length){
+  if(Math.abs(focusIdTop)>imagesTop.length || focusIdTop == 1){
     sliderGalleryTop.style.transition = 'none';
     sliderGalleryTop.style.transform = `translateX(-${arrayWidthTop*2}px)`;
     focusIdTop = 1;
@@ -48,7 +52,7 @@ sliderGalleryTop.addEventListener('transitionend', () => {
 })
 
 sliderGalleryBottom.addEventListener('transitionend', () => {
-  if(Math.abs(focusIdBottom)>imagesBottom.length){
+  if(Math.abs(focusIdBottom)>imagesBottom.length || focusIdBottom == 1){
     sliderGalleryBottom.style.transition = 'none';
     sliderGalleryBottom.style.transform = `translateX(-${arrayWidthBottom*2}px)`;
     focusIdBottom = 1;
@@ -63,8 +67,8 @@ function setInitialGalleryState(){
   const galleryTop = document.querySelector('.slider__gallery--top').innerHTML;
   const galleryBottom = document.querySelector('.slider__gallery--bottom').innerHTML;
 
-  sliderGalleryTop.innerHTML = galleryTop + galleryTop;
-  sliderGalleryBottom.innerHTML = galleryBottom + galleryBottom;
+  sliderGalleryTop.innerHTML = galleryTop + galleryTop + galleryTop;
+  sliderGalleryBottom.innerHTML = galleryBottom + galleryBottom + galleryBottom;
 
   resetPosition();
 }
@@ -72,7 +76,6 @@ function setInitialGalleryState(){
 function calculateWidthTop(){
   let sum = 0;
   imagesTop.forEach(img => sum += (img.width)+10);
-  console.log("top" + sum);
   return sum;
 }
 
@@ -86,4 +89,77 @@ function calculateWidthBottom(){
 function resetPosition(){
   sliderGalleryTop.style.transform = `translateX(-${arrayWidthTop*2}px)`;
   sliderGalleryBottom.style.transform = `translateX(-${arrayWidthBottom*2}px)`;
+}
+
+function moveRowBack(){
+  if(focusIdTop == imagesTop.length){
+    focusIdTop = 1;
+    sliderGalleryTop.style.transform = `translateX(-${arrayWidthTop*2}px)`;
+  } else {
+    if(focusIdTop > 1){
+      offsetTop += imagesTop[focusIdTop-1].width/2 +10;
+      sliderGalleryTop.style.transform = `translateX(-${arrayWidthTop*2-offsetTop}px)`;
+      focusIdTop++;
+
+    } else {
+      offsetTop += imagesTop[Math.abs(focusIdTop)-1].width/2 +10;
+      sliderGalleryTop.style.transform = `translateX(-${arrayWidthTop*2-offsetTop}px)`;
+      focusIdTop == 1 ? focusIdTop = -2 : focusIdTop--;
+    }
+  }
+
+  if(focusIdBottom == imagesBottom.length){
+    focusIdBottom = 1;
+    sliderGalleryBottom.style.transform = `translateX(-${arrayWidthBottom*2}px)`;
+  }else {
+    if(focusIdBottom > 1){
+      offsetBottom += imagesBottom[focusIdBottom-1].width/2 +10;
+      sliderGalleryBottom.style.transform = `translateX(-${arrayWidthBottom*2-offsetBottom}px)`;
+      focusIdBottom++;
+    } else {
+      offsetBottom += imagesBottom[Math.abs(focusIdBottom)-1].width/2 +10;
+      sliderGalleryBottom.style.transform = `translateX(-${arrayWidthBottom*2-offsetBottom}px)`;
+      focusIdBottom == 1 ? focusIdBottom = -2 : focusIdBottom--;
+    }
+  }
+
+  setTimeout(function(){
+    isDisabledBack = false;
+    btnPrev.classList.remove('disabled');
+    btnPrev.src = "./assets/images/arrow-blue-left.png";
+  },600);
+}
+
+function moveRowForward() {
+  if(Math.abs(focusIdTop) == 1) focusIdTop = imagesTop.length+1;
+  if(Math.abs(focusIdBottom) == 1) focusIdBottom = imagesBottom.length+1;
+
+  if(focusIdTop > 0){
+    focusIdTop--;
+    offsetTop -= imagesTop[Math.abs(focusIdTop)-1].width/2 +10;
+    sliderGalleryTop.style.transform = `translateX(-${arrayWidthTop*2-offsetTop}px)`;
+  } else {
+    focusIdTop++;
+    offsetTop -= imagesTop[Math.abs(focusIdTop)-1].width/2 +10;
+    sliderGalleryTop.style.transform = `translateX(-${arrayWidthTop*2-offsetTop}px)`;
+
+  }
+
+  if(focusIdBottom > 0){
+    focusIdBottom--;
+    offsetBottom -= imagesBottom[Math.abs(focusIdBottom)-1].width/2 +10;
+    sliderGalleryBottom.style.transform = `translateX(-${arrayWidthBottom*2-offsetBottom}px)`;
+
+  } else {
+    focusIdBottom++;
+    offsetBottom -= imagesBottom[Math.abs(focusIdBottom)-1].width/2 +10;
+    sliderGalleryBottom.style.transform = `translateX(-${arrayWidthBottom*2-offsetBottom}px)`;
+  }
+
+  setTimeout(function(){
+    isDisabledForward = false;
+    btnNext.classList.remove('disabled');
+    btnNext.src = "./assets/images/arrow-blue-right.png";
+  },600);
+
 }
